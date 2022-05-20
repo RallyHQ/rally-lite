6c8461e8e7847fb105700a9bf522d38e81cdbdd2
Alec Robins <alecrobins@gmail.com>
2022-05-20 07:49:41 -0500

    Use fetch in getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -135,18 +135,18 @@ const IndexPage = ({ user }: Props) => {
       </div>
       <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
         <div className="flex-1 flex justify-between sm:hidden">
-          <a
-            href="#"
+          <button
+            onClick={() => {}}
             className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Previous
-          </a>
-          <a
-            href="#"
+          </button>
+          <button
+            onClick={() => {}}
             className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Next
-          </a>
+          </button>
         </div>
         <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
           <div>
@@ -161,60 +161,59 @@ const IndexPage = ({ user }: Props) => {
               className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
               aria-label="Pagination"
             >
-              <a
-                href="#"
+              <button
+                onClick={() => {}}
                 className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
               >
                 <span className="sr-only">Previous</span>
                 <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </a>
-              <a
-                href="#"
+              </button>
+              <button
                 aria-current="page"
                 className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
               >
                 1
-              </a>
-              <a
-                href="#"
+              </button>
+              <button
+                onClick={() => {}}
                 className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
               >
                 2
-              </a>
-              <a
-                href="#"
+              </button>
+              <button
+                onClick={() => {}}
                 className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
               >
                 3
-              </a>
+              </button>
               <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
                 ...
               </span>
-              <a
-                href="#"
+              <button
+                onClick={() => {}}
                 className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
               >
                 8
-              </a>
-              <a
-                href="#"
+              </button>
+              <button
+                onClick={() => {}}
                 className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
               >
                 9
-              </a>
-              <a
-                href="#"
+              </button>
+              <button
+                onClick={() => {}}
                 className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
               >
                 10
-              </a>
-              <a
-                href="#"
+              </button>
+              <button
+                onClick={() => {}}
                 className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
               >
                 <span className="sr-only">Next</span>
                 <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </a>
+              </button>
             </nav>
           </div>
         </div>
@@ -224,14 +223,11 @@ const IndexPage = ({ user }: Props) => {
 };
 
 export async function getServerSideProps() {
-  const user = await prisma.user.findFirst();
+  const res = await fetch('http://localhost:3000/api/user');
+  const user = await res.json();
   return {
     props: {
-      user: {
-        ...user,
-        createdAt: new Date(user?.createdAt).valueOf(),
-        updatedAt: new Date(user?.createdAt).valueOf(),
-      },
+      user,
     },
   };
 }
