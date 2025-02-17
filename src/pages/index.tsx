fd7bf51655dfe3f60eb171acd3aba1c9f5712f1d
Alec Robins <alecrobins@gmail.com>
2025-02-17 14:06:04 -0500

    Update index.tsx
    Remove mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -135,20 +135,6 @@ const IndexPage = ({ user }: Props) => {
         </div>
       </div>
       <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-        <div className="flex-1 flex justify-between sm:hidden">
-          <button
-            onClick={() => {}}
-            className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-          >
-            Previous
-          </button>
-          <button
-            onClick={() => {}}
-            className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-          >
-            Next
-          </button>
-        </div>
         <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
           <div>
             <p className="text-sm text-gray-700">
